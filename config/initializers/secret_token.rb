d8e81abd18854032e3567bbe94aa3c34ca4974a6
me <me@stuffihavemade.com>
2011-11-25 11:41:45 -0500

    This works. The twitter bootstrap less files are being directly imported into a css.less file (home.css.less).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LessRailsTest::Application.config.secret_token = '0539032dd2d7bae9e114c81b8f892a825c5003f392bcc1afcf056cccb33c5f7d361a5b94ddd0a00e96dbc36e918de37ba90ff01a957cae300d17124b0a69c5a7'
